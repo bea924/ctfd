f552114c7a6d90465ed2a378a09a07f1dfbc2957
bea924 <beatricepicco@outlook.com>
2024-07-02 13:16:56 +0200

    ex6 HeatEq_2D_point

--- ex6_green/ex6_func.py
+++ ex6_green/ex6_func.py
@@ -105,3 +105,55 @@ def return_GF_1D_array(x, x_s_start, x_s_end, Lx, bc_in: str, bc_out: str, n_ste
             GF_matrix[i] = GF_1D_function(x, x_s[i], Lx, bc_in = bc_in, bc_out = bc_out)
 
     return GF_matrix
+
+
+def HeatEq_2D_point(x, y, omega, lambda_v, Lx, Ly, x_s_start, x_s_end, y_s_start, y_s_end, bc_e: str, bc_n: str, bc_w: str, bc_s: str, T, q, n_step_s = 21):
+    x_s = np.linspace(x_s_start, x_s_end, n_step_s)
+    y_s = np.linspace(y_s_start, y_s_end, n_step_s)
+
+    # 2D GF integral over surface
+    GF_S = return_GF_matrix(x, y, x_s_start, x_s_end, y_s_start, y_s_end, Lx, Ly, bc_e, bc_n, bc_w, bc_s) # 2D GF(x_s, y_s)
+    integral_GF_ys = np.trapz(GF_S*omega, y_s, axis=0) #over y
+    integral_GF_ys_xs = np.trapz(integral_GF_ys, x_s) #over x
+    integral_GF_ys_xs = integral_GF_ys_xs * (1/lambda_v)
+
+    # 1D GF as a function of xs and ys
+    GF_x = return_GF_1D_array(x, x_s_start, x_s_end, Lx, bc_e, bc_w) # 1D GF(x_s)
+    GF_y = return_GF_1D_array(y, y_s_start, x_s_end, Ly, bc_s, bc_n) # 1D GF(y_s)
+
+    # East integral
+    if (bc_e == "D"): # Dirichlet
+        dGdx_E = np.zeros(n_step_s)
+        dGdx_E[1:] = np.diff(GF_y) / np.diff(x_s)
+        integral_GF_E = np.trapz(dGdx_E*T, y_s) #over y
+    elif (bc_e == "N"): # Neumann
+        .
+
+    # North integral
+    if (bc_n == "D"): # Dirichlet
+        GF_x_N = GF_x[::-1] # swap because of line direction
+        dGdy_N = np.zeros(n_step_s)
+        dGdy_N[1:] = np.diff(GF_x_N) / np.diff(y_s)
+        integral_GF_N = np.trapz(dGdy_N*T, x_s) #over x
+    elif (bc_n == "N"): # Neumann
+        .
+
+    # West integral
+    if (bc_w == "D"): # Dirichlet
+        GF_y_W = GF_y[::-1] # swap because of line direction
+        dGdx_W = np.zeros(n_step_s)
+        dGdx_W[1:] = np.diff(GF_y_W) / np.diff(x_s)
+        integral_GF_W = np.trapz(dGdx_W*T, y_s) #over y
+    elif (bc_w == "N"): # Neumann
+        .
+
+    # South integral
+    if (bc_s == "D"): # Dirichlet
+        dGdy_S = np.zeros(n_step_s)
+        dGdy_S[1:] = np.diff(GF_x) / np.diff(y_s)
+        integral_GF_S = np.trapz(dGdy_S*T, x_s) #over x
+    elif (bc_s == "N"): # Neumann
+        .
+
+    T_xy = integral_GF_ys_xs + integral_GF_E + integral_GF_N + integral_GF_W + integral_GF_S
+    return T_xy
