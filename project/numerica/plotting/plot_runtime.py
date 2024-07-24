e315cd87846ffceafa0328501e266f6eca57e6f0
bea924 <beatricepicco@outlook.com>
2024-07-24 16:52:59 +0200

    plot runtime of different solvers

--- project/numerica/plotting/plot_runtime.py
+++ project/numerica/plotting/plot_runtime.py
@@ -5,38 +5,34 @@ import matplotlib.pyplot as plt
 # 0 exact
 # 1 lax
 # 2 roe
-solver = 0
+solver_list = [0, 1, 2]
 output_time = 0.2
 
-# Read the .out file
-file_path = f'output/solver{solver}_t{output_time}.out'
-data = pd.read_csv(file_path, delim_whitespace=True, header=None)
 
-# Print the data to verify
-print(data)
+fig, axes = plt.subplots(1, 1, figsize=(8, 6))
+solver_dict = {
+    0: 'Exact',
+    1: "Lax",
+    2: 'Roe'
+}
 
-columns = np.zeros((data.shape[1],data.shape[0]))
-# Access columns
-columns[0] = data[0].to_numpy()
-columns[1] = data[1].to_numpy()
-columns[2] = data[2].to_numpy()
-columns[3] = data[3].to_numpy()
-columns[4] = data[4].to_numpy()
+for i, solver in enumerate(solver_list):
+    # Read the .out file
+    file_path = f'output/solver{solver}_t{output_time}_stats.out'
+    # file_path = f'project/numerica/output/solver{solver}_t{output_time}_stats.out' # for debugger
+    runtime = pd.read_csv(file_path, delim_whitespace=True, header=None)
 
-columns_names = ["Density", "Velocity", "Pressure", "Energy"]
+    runtime = runtime[0].to_numpy()
+    x = np.arange(0,len(runtime))
+
+    # Plot each column in a separate subplot
+    axes.plot(x, runtime, label=solver_dict[i])
+    # axes.set_title(f'Plot of {solver_dict[i]}')
+    axes.legend()
+    axes.grid(True)
 
 
-# Create a figure and subplots
-fig, axes = plt.subplots(4, 1, figsize=(6, 8))
-x = np.arange(0,100)
-
-# Plot each column in a separate subplot
-for i in range(4):
-    axes[i].plot(columns[0], columns[i+1], label=columns_names[i])
-    axes[i].set_title(f'Plot of {columns_names[i]}')
-    axes[i].legend()
-    axes[i].grid(True)
-
+axes.set_title(f'Plot of runtime')
 # Adjust layout
 plt.tight_layout()
 
