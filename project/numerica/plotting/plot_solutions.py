8ed0e86e0654b00e5c687be6f93408d23bb06697
bea924 <beatricepicco@outlook.com>
2024-07-31 19:24:13 +0200

    2d plot

--- project/numerica/plotting/plot_solutions.py
+++ project/numerica/plotting/plot_solutions.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import os
-from plotting.error_calculation import MSE_calculate
+
+def MSE_calculate(error_absolute):
+    pow = error_absolute**2
+    return np.mean(pow)
 
 # plot style
 plt.rcParams.update({'font.size': 14})
@@ -228,7 +231,7 @@ def plot_solution_stability(problem_type, solver_list, n_cells, output_time_list
 
             axes[i//2, i%2].plot(columns[0], columns[1], label=f"{solver_name_dict[solver]}", color=solver_color_dict[solver], linewidth=2)
             axes[i//2, i%2].set_xlabel('x')
-            axes[i//2, i%2].set_ylabel(f'{columns_names[i]}')
+            axes[i//2, i%2].set_ylabel(f'{columns_names[0]}')
             axes[i//2, i%2].grid(True)
             axes[i//2, i%2].set_title(f'Output time {output_time_list[i]}')
 
@@ -320,7 +323,7 @@ def plot_solutions_compare(problem_type, solver_list, n_cells, output_time):
     with open(jsonfile_path, 'r') as file:
         analytic_solution = json.load(file)
 
-    fig, axes = plt.subplots(2, 2, figsize=(7, 6))
+    fig, axes = plt.subplots(2, 2, figsize=(8, 7))
 
     for j, solver in enumerate(solver_list):
         # Read the .out file
@@ -352,7 +355,7 @@ def plot_solutions_compare(problem_type, solver_list, n_cells, output_time):
 
     handles, labels = axes[0, 0].get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper center', ncol=2)
-    plt.tight_layout(rect=[0, 0, 1, 0.9])
+    plt.tight_layout(rect=[0, 0, 1, 0.85])
     plt.show()
 
 
@@ -389,26 +392,54 @@ def plot_2d_solution(problem_type, solver, n_cells, output_time_start, output_ti
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     output_time_array = np.arange(output_time_start, output_time_end+output_time_step, output_time_step)
-    solution_2d = np.zeros((len(output_time_array), 100))
+    solution_2d = np.zeros((len(output_time_array), n_cells))
 
     for i, output_time in enumerate(output_time_array):
         # Read the .out file
         file_path = os.path.join(script_dir, '..', f"output/{problem_type}", f'solver{solver}_t{output_time:.3f}_n{n_cells}.out')
         data = pd.read_csv(file_path, sep='\s+', header=None)
-
-        # Read data into columns
-        columns = np.zeros((data.shape[1],data.shape[0]))
-        for i in range(5):
-            columns[i] = data[i].to_numpy()
-
-        solution_2d[i] = columns[2] # velocity
+        solution_2d[i] = data[2].to_numpy() # velocity
 
     # Color map
     plt.imshow(solution_2d, cmap='viridis')
     plt.gca().invert_yaxis()
     colorbar = plt.colorbar(location='bottom')
     colorbar.set_label('Velocity')
-    plt.gca().set_yticks([0, 0.05, 0.4])
+    # plt.gca().set_yticks([0, 0.05, 0.4])
     plt.xlabel('X')
     plt.ylabel('Output time')
     plt.show()
+
+
+
+def plot_solutions_compare_return(problem_type, solver_list, n_cells, output_time):
+    # Get current directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    fig, axes = plt.subplots(2, 2, figsize=(8, 7))
+
+    for j, solver in enumerate(solver_list):
+        # Read the .out file
+        file_path = os.path.join(script_dir, '..', f"output/{problem_type}", f'solver{solver}_t{output_time:.3f}_n{n_cells}.out')
+        data = pd.read_csv(file_path, sep='\s+', header=None)
+        
+        # Access columns
+        columns = np.zeros((data.shape[1], data.shape[0]))
+        for i in range(5):
+            columns[i] = data[i].to_numpy()
+        columns_names = ["Density", "Velocity", "Pressure", "Energy"]
+
+        # Plot each column in a separate subplot
+        for i in range(4):
+            axes[i//2, i%2].plot(columns[0], columns[i+1], label=f"{solver_name_dict[solver]}", color=solver_color_dict[solver])
+
+    for i in range(4):
+        # Plot the analytic solution
+        axes[i//2, i%2].set_xlabel('x')
+        axes[i//2, i%2].set_ylabel(f'{columns_names[i]}')
+        axes[i//2, i%2].grid(True)
+
+    handles, labels = axes[0, 0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', ncol=2)
+    plt.tight_layout(rect=[0, 0, 1, 0.85])
+    return fig, axes
